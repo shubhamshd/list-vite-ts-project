4746198545c51b05f911b4d23e2b2b2921b064d8
Shubham Dubey <shubhamshd99@gmail.com>
2023-08-23 15:39:48 +0530

    added better implementation for the render function

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -26,26 +26,58 @@ export default class ListTemplate implements DomList {
 
     // let ulInnerHtml = ''
     fullList.list.forEach(listItem => {
-      const ulInnerHtml = `<li class="item">
-        <input type="checkbox" id=${listItem.id} tabIndex = 0 checked=${listItem.checked}>
-        <label for=${listItem.id}>${listItem.item}</label>
-        <button id="button-${listItem.id}" class="button">X</button>
-      </li>`
 
-      this.ul.innerHTML += ulInnerHtml
+      const li = document.createElement('li')
+      li.className = 'item'
 
-      const inputElement = document.getElementById(listItem.id) as HTMLInputElement
-      inputElement.addEventListener('change', () => {
+      const input = document.createElement('input')
+      input.type = 'checkbox'
+      input.id = listItem.id
+      input.tabIndex = 0
+      input.checked = listItem.checked
+      li.append(input)
+
+      input.addEventListener('change', () => {
         listItem.checked = !listItem.checked      
         fullList.save()
       })
 
-      const buttonElement = document.getElementById(`button-${listItem.id}`) as HTMLButtonElement
+      const label = document.createElement('label')
+      label.htmlFor = listItem.id
+      label.textContent = listItem.item
+      li.append(label)
 
-      buttonElement.addEventListener('click', () => {
+      const button = document.createElement('button')
+      button.className = 'button'
+      button.textContent = 'X'
+      li.append(button)
+
+      button.addEventListener('click', () => {
         fullList.removeItem(listItem.id)
         this.render(fullList)
       })
+
+      ////////// another implementation which is not so optimised
+      // const ulInnerHtml = `<li class="item">
+      //   <input type="checkbox" id=${listItem.id} tabIndex = 0 checked=${listItem.checked}>
+      //   <label for=${listItem.id}>${listItem.item}</label>
+      //   <button id="button-${listItem.id}" class="button">X</button>
+      // </li>`
+
+      // this.ul.innerHTML += ulInnerHtml
+
+      // const inputElement = document.getElementById(listItem.id) as HTMLInputElement
+      // inputElement.addEventListener('change', () => {
+      //   listItem.checked = !listItem.checked      
+      //   fullList.save()
+      // })
+
+      // const buttonElement = document.getElementById(`button-${listItem.id}`) as HTMLButtonElement
+
+      // buttonElement.addEventListener('click', () => {
+      //   fullList.removeItem(listItem.id)
+      //   this.render(fullList)
+      // })
     })
   }
 }
